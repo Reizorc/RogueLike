1914e47f65b6516d19bc88cb9e1baa1a48305a40
Reizorc <reizorc77@gmail.com>
2016-09-26 21:43:21 +0200

    Score

---
+++ Space Shooter/Space Shooter/missile.cpp
@@ -0,0 +1,16 @@
+#include "missile.h"
+
+missile::missile()
+{
+    rect.setSize(sf::Vector2f(5,10));
+    rect.setFillColor(sf::Color::Red);
+    isDestroyed = false;
+}
+
+void missile::updatePosition(){
+    if(rect.getPosition().y >=-9){
+            rect.move(0,-moveSpeed);
+    }else{
+        isDestroyed = true;
+    }
+}
